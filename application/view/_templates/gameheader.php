bb50f5fd70a5e747b898c4ae0b09bd7c6ce77521
BenKemp96 <99031541@mydavinci.nl>
2016-06-14 12:09:48 +0200

    visual overhaul

--- application/view/_templates/gameheader.php
+++ application/view/_templates/gameheader.php
@@ -10,3 +10,4 @@
     <script src="<?php echo Config::get('URL'); ?>js/jquery-1.12.4.min.js"></script>
     <script src="<?php echo Config::get('URL'); ?>js/jquery-ui.min.js"></script>
 </head>
+<body id="gameBody">
--- application/view/game/index.php
+++ application/view/game/index.php
@@ -1,10 +1,14 @@
-<img id="fire" class="draggable" src="<?php echo Config::get('URL'); ?>_img/materials/fire.png">
-<img id="air" class="draggable" src="<?php echo Config::get('URL'); ?>_img/materials/air.png">
+<img class="circle" src="<?php echo Config::get('URL'); ?>_img/alpha.gif">
+<img class="circle" src="<?php echo Config::get('URL'); ?>_img/beta.gif">
+<img class="circle" src="<?php echo Config::get('URL'); ?>_img/gamma.gif">
+<img class="circle" src="<?php echo Config::get('URL'); ?>_img/delta.gif">
+<img class="circle" src="<?php echo Config::get('URL'); ?>_img/omega.gif">
 
 <div id="combiner">
-    <img id="combinerContainer" src="<?php echo Config::get('URL'); ?>_img/Combine2.png" usemap="#combinerMap">
+    <img id="combinerContainer" src="<?php echo Config::get('URL'); ?>_img/Combiner2.png" usemap="#combinerMap">
     <map id="combinerMap" name="combinerMap">
 	    <area shape="rect" coords="56,34,195,337" target="_self" class="droppable">
 	    <area shape="rect" coords="348,34,487,337" target="_self" class="droppable">
 	</map>
+	<button onclick="returnMaterials()">Return</button>
 </div>

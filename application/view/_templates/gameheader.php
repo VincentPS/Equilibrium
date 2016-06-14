583b0158862dc63b5d63607deb5b22ba3cd38f04
VincentPS <99026400@mydavinci.nl>
2016-06-14 13:03:54 +0200

    Login Style and Register Style

--- application/view/_templates/gameheader.php
+++ application/view/_templates/gameheader.php
@@ -9,4 +9,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <script src="<?php echo Config::get('URL'); ?>js/jquery-1.12.4.min.js"></script>
     <script src="<?php echo Config::get('URL'); ?>js/jquery-ui.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Itim' rel='stylesheet' type='text/css'>
 </head>

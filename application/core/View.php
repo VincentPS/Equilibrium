3ec56ae302046733f34a04c1cf45dcb1b1c6847c
BenKemp96 <99031541@mydavinci.nl>
2016-06-16 08:58:19 +0200

    game index fix

--- application/core/View.php
+++ application/core/View.php
@@ -15,8 +15,14 @@ class View
         require Config::get('PATH_VIEW') . '_templates/footer.php';
     }
 
-    public function renderGame($filename)
+    public function renderGame($filename, $data = null)
     {
+        if ($data) {
+            foreach ($data as $key => $value) {
+                $this->{$key} = $value;
+            }
+        }
+
         require Config::get('PATH_VIEW') . '_templates/gameheader.php';
         require Config::get('PATH_VIEW') . $filename . '.php';
         require Config::get('PATH_VIEW') . '_templates/gamefooter.php';
--- application/view/_templates/gameheader.php
+++ application/view/_templates/gameheader.php
@@ -1,14 +1,19 @@
+<!doctype html>
+<html>
 <head>
     <title>Equilibrium</title>
+
     <meta charset="utf-8">
     <meta name="description" content="Equilibrium is a puzzle-game built within the browser">
     <meta name="keywords" content="Puzzle, Game, Webbased, Equilibrium">
     <meta name="author" content="Ben Kemp, Vincent Schaddelee">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
     <link rel="icon" href="<?php echo Config::get('URL'); ?>favicon.png">
     <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/game.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href='https://fonts.googleapis.com/css?family=Itim' rel='stylesheet' type='text/css'>
+
     <script src="<?php echo Config::get('URL'); ?>js/jquery-1.12.4.min.js"></script>
     <script src="<?php echo Config::get('URL'); ?>js/jquery-ui.min.js"></script>
-    <link href='https://fonts.googleapis.com/css?family=Itim' rel='stylesheet' type='text/css'>
 </head>
 <body id="gameBody">

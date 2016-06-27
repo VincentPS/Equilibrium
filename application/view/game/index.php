d93f09f51874c6d181e11ff964281284c29d2fef
BenKemp96 <99031541@mydavinci.nl>
2016-06-27 11:30:52 +0200

    game positioning

--- application/view/game/index.php
+++ application/view/game/index.php
@@ -1,61 +1,12 @@
 <ul class="circle">
-  <li class="alpha"><img src="<?php echo Config::get('URL'); ?>_img/alpha.gif">
-    <ul class="innerAlpha">
-	    <?php foreach ($this->materials as $material) {
-			if ($material->circle == "A") {
-				$mat_name = strtolower($material->mat_name);
-				echo "<li><img class='draggable " . $mat_name . "' src=" . Config::get('URL') . "public/_img/materials/" . $mat_name . ".png></li>";
-			}
-		} ?>
-    </ul>
-  </li>
-  <li class="beta"><img src="<?php echo Config::get('URL'); ?>_img/beta.gif">
-    <ul class="innerBeta">
-      	<?php foreach ($this->materials as $material) {
-			if ($material->circle == "B") {
-				$mat_name = strtolower($material->mat_name);
-				echo "<li><img class='draggable " . $mat_name . "' src=" . Config::get('URL') . "public/_img/materials/" . $mat_name . ".png></li>";
-			}
-		} ?>
-    </ul>
-  </li>
-  <li class="gamma"><img src="<?php echo Config::get('URL'); ?>_img/gamma.gif">
-    <ul class="innerGamma">
-     	<?php foreach ($this->materials as $material) {
-			if ($material->circle == "G") {
-				$mat_name = strtolower($material->mat_name);
-				echo "<li><img class='draggable " . $mat_name . "' src=" . Config::get('URL') . "public/_img/materials/" . $mat_name . ".png></li>";
-			}
-		} ?>
-    </ul>
-  </li>
-  <li class="delta"><img src="<?php echo Config::get('URL'); ?>_img/delta.gif">
-    <ul class="innerDelta">
-      	<?php foreach ($this->materials as $material) {
-			if ($material->circle == "D") {
-				$mat_name = strtolower($material->mat_name);
-				echo "<li><img class='draggable " . $mat_name . "' src=" . Config::get('URL') . "public/_img/materials/" . $mat_name . ".png></li>";
-			}
-		} ?>
-    </ul>
-  </li>
-  <li class="omega"><img src="<?php echo Config::get('URL'); ?>_img/omega.gif">
-    <ul class="innerOmega">
-      	<?php foreach ($this->materials as $material) {
-			if ($material->circle == "O") {
-				$mat_name = strtolower($material->mat_name);
-				echo "<li><img class='draggable " . $mat_name . "' src=" . Config::get('URL') . "public/_img/materials/" . $mat_name . ".png></li>";
-			}
-		} ?>
-    </ul>
-  </li>
+  <li class="alpha"><img src="<?php echo Config::get('URL'); ?>_img/alpha.gif"></li>
+  <li class="beta"><img src="<?php echo Config::get('URL'); ?>_img/beta.gif"></li>
+  <li class="gamma"><img src="<?php echo Config::get('URL'); ?>_img/gamma.gif"></li>
+  <li class="delta"><img src="<?php echo Config::get('URL'); ?>_img/delta.gif"></li>
+  <li class="omega"><img src="<?php echo Config::get('URL'); ?>_img/omega.gif"></li>
 </ul>
 
 <div id="combiner">
-    <img id="combinerContainer" src="<?php echo Config::get('URL'); ?>_img/Combiner2.png" usemap="#combinerMap">
-    <map id="combinerMap" name="combinerMap">
-	    <area shape="rect" coords="60,59,273,531" class="droppable area1">
-	    <area shape="rect" coords="520,59,732,531" class="droppable area2">
-	</map>
+    <img id="combinerContainer" src="<?php echo Config::get('URL'); ?>_img/Combiner2.png">
 	<button id="return" onclick="returnMaterials()">Return</button>
 </div>

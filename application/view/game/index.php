7ff9c8bfea0ffdd4b5a28f3944442aa446b8d2cc
VincentPS <99026400@mydavinci.nl>
2016-06-29 09:55:32 +0200

    Css fixed

--- application/view/game/index.php
+++ application/view/game/index.php
@@ -1,12 +1,13 @@
-<ul class="circle">
-  <li class="alpha"><button onclick="openAlpha()"><img src="<?php echo Config::get('URL'); ?>_img/alpha.gif"></button></li>
-  <li class="beta"><button onclick="openBeta()"><img src="<?php echo Config::get('URL'); ?>_img/beta.gif"></button></li>
-  <li class="gamma"><button onclick="openGamma()"><img src="<?php echo Config::get('URL'); ?>_img/gamma.gif"></button></li>
-  <li class="delta"><button onclick="openDelta()"><img src="<?php echo Config::get('URL'); ?>_img/delta.gif"></button></li>
-  <li class="omega"><button onclick="openOmega()"><img src="<?php echo Config::get('URL'); ?>_img/omega.gif"></button></li>
-</ul>
-
-<div class="materials"></div>
+<section class="pageLeft">
+  <div class="materials"></div>
+  <ul class="circle">
+    <li class="alpha"><button onclick="openAlpha()"><img src="<?php echo Config::get('URL'); ?>_img/alpha.gif"></button></li>
+    <li class="beta"><button onclick="openBeta()"><img src="<?php echo Config::get('URL'); ?>_img/beta.gif"></button></li>
+    <li class="gamma"><button onclick="openGamma()"><img src="<?php echo Config::get('URL'); ?>_img/gamma.gif"></button></li>
+    <li class="delta"><button onclick="openDelta()"><img src="<?php echo Config::get('URL'); ?>_img/delta.gif"></button></li>
+    <li class="omega"><button onclick="openOmega()"><img src="<?php echo Config::get('URL'); ?>_img/omega.gif"></button></li>
+  </ul>
+</section>
 
 <div id="combiner">
     <img id="combinerContainer" src="<?php echo Config::get('URL'); ?>_img/Combiner3.png">
